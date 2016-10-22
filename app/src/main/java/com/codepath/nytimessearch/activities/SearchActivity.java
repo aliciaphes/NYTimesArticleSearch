62757be6fee8a1c433c81dc4f9e3ca9bd881c516
ALicia P <pericia@gmail.com>
2016-10-22 13:02:08 -0400

    Start defining the action bar

--- app/src/main/java/com/codepath/nytimessearch/activities/SearchActivity.java
+++ app/src/main/java/com/codepath/nytimessearch/activities/SearchActivity.java
@@ -78,12 +78,15 @@ public class SearchActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id) {
+            case R.id.filter:
+                //show dialog/activity with filtering options
+                return true;
+            case R.id.action_settings:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 
     public void onArticleSearch(View view) {
